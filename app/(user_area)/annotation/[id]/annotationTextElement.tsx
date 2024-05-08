0a02cd41db9ee5e68d4ab756aebf881171d6a5af
Lasse Jantsch <lassejantsch@web.de>
2024-05-08 10:09:55 +0900

    implemented multi annotations tool

--- app/(user_area)/annotation/[id]/annotationTextElement.tsx
+++ app/(user_area)/annotation/[id]/annotationTextElement.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 
-const AnnotationTextElement = ({id, i, s, setShowInfoCard}: {id: string, i: number, s: string, setShowInfoCard: React.Dispatch<React.SetStateAction<boolean>>}) => {
+const AnnotationTextElement = ({id, i, s, mark, setShowInfoCard}: {id: string, i: number, s: string, mark: number, setShowInfoCard: React.Dispatch<React.SetStateAction<boolean>>}) => {
     var classList = 'text'
     if (s.includes('TREF]')){
         classList += ' target'
     }
     if (s.includes('REF]')) {
         classList += ' reference'
+    }
+    switch(mark){
+        case 0:
+            break
+        case 1:
+            classList += ' marked info'
+            break
+        case 2:
+            classList += ' marked judge'
+            break
+        case 3:
+            classList += ' marked backgr'
+            break
     }
 
     return(
